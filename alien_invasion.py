cf0682259a4946a48fcb899113822cf9d1231749
DomicChan <1778233373@qq.com>
2017-08-21 20:25:20 +0800

    重构：添加函数update_screen()及修正错误

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,24 +8,16 @@ def run_game():
 	# 初始化游戏并创建一个屏幕对象
 	pygame.init()
 	ai_settings = Settings()
-	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+	screen = pygame.display.set_mode((ai_settings.screen_width,
+	                                  ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
 
 	# 创建一艘飞船
 	ship = Ship(screen)
 
-    # 设置背景色
-	bg_color = (ai_settings.bg_color)  # 浅灰色
-
 	# 开始游戏的主循环
 	while True:
 		gf.check_events()
-
-        # 每次循环时都重绘屏幕
-		screen.fill(bg_color)
-		ship.blitme()
-
-		# 让最近绘制的屏幕可见
-		pygame.display.flip()
+		gf.update_screen(ai_settings, screen, ship)
 
 run_game()
